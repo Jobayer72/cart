1f708d0098597fcdd645f0fa5c65136714c38fdc
Jobayer72 <jobayer722021@gmail.com>
2021-10-03 23:16:24 +0600

    cart system

---
+++ Data.js
@@ -0,0 +1,30 @@
+import img1 from './img/img1.jpg'
+import img2 from './img/img2.jpg'
+import img3 from './img/img3.jpg'
+
+const Data = {
+    productData : [
+        {
+            id : 1,
+            img : img1,
+            title : "Django web",
+            desc : "",
+            price : 46,
+        },
+        {
+            id : 2,
+            img : img2,
+            title : " Frontend",
+            desc : "",
+            price : 30,
+        },
+        {
+            id : 3,
+            img : img3,
+            title : "Backend",
+            desc : "",
+            price : 20,
+        }
+    ]
+}
+export default Data
